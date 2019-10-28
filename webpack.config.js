fe1c4acfdd886340ea4ad748a5a52ff1930c9df4
zam <ajmzamora@gmail.com>
2019-10-28 00:00:10 -0400

    chore(webpack): add style loaders; update config
    style plugins/loaders: mini-css-extract-plugin, sass-loader, node-sass, css-loader

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,6 @@
 // webpack.config.js
 const path = require('path');
-
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 
 
 // $config: common config for development and production
@@ -9,6 +9,7 @@ let config = {
     entry: './src/js/index.js',
     module: {
         rules: [
+            // js loader
             {
               test: /\.m?js$/,
               exclude: /(node_modules|bower_components)/,
@@ -19,6 +20,26 @@ let config = {
                   plugins: ['@babel/plugin-proposal-object-rest-spread'],
                 },
               },
+            },
+            // css loader
+            {
+              test: /\.s[ac]ss$/i,
+              use: [
+                {
+                  loader: MiniCssExtractPlugin.loader,
+                  options: {
+                    publicPath: (resourcePath, context) => {
+                      // publicPath is the relative path of the resource to the context
+                      // e.g. for ./css/admin/main.css the publicPath will be ../../
+                      // while for ./css/main.css the publicPath will be ../
+                      return path.relative(path.dirname(resourcePath), context) + '/';
+                    },
+                  }
+                }, //3. Extract css into files
+                // "style-loader", //3. Inject styles into DOM
+                "css-loader", //2. Turns css into commonjs
+                "sass-loader" //1. Turns sass into css
+              ],
             },
         ],
     },
@@ -34,6 +55,10 @@ module.exports = (env, argv) => {
       chunkFilename: 'js/[id].bundle.js',
       publicPath: '../',
     };
+
+    config.plugins = [
+      new MiniCssExtractPlugin({ filename: "css/[name].css" }),
+    ];
   }
 
   else if (argv.mode === 'production') {
@@ -43,6 +68,10 @@ module.exports = (env, argv) => {
       chunkFilename: 'js/[id]bundle.js',
       publicPath: '../',
     };
+
+    config.plugins = [
+      new MiniCssExtractPlugin({ filename: "css/[name].[contentHash:11].css" }),
+    ];
   }
 
   return config;
